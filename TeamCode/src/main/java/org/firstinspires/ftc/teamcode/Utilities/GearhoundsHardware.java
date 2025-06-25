2c359e7f55a97716bdbb2ee1d664af15cc439306
hmock <hmock@ycds.org>
2025-06-25 13:51:37 -0400

    Add initial robot files and start ServoTest.java
    - Added a cleaned-up GearhoundsHardware.java
    - Introduced Hardware.java to connect with rest of code
    - Added blank ServoTest.java for future implementation

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Utilities/GearhoundsHardware.java
@@ -0,0 +1,108 @@
+package org.firstinspires.ftc.teamcode.Utilities;
+
+//import com.qualcomm.hardware.bosch.BNO055IMU i
+import com.qualcomm.hardware.rev.RevHubOrientationOnRobot;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorEx;
+import com.qualcomm.robotcore.hardware.DcMotorImplEx;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+import com.qualcomm.robotcore.hardware.IMU;
+import com.qualcomm.robotcore.hardware.Servo;
+
+import org.firstinspires.ftc.robotcore.external.navigation.Acceleration;
+import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
+import org.firstinspires.ftc.robotcore.external.navigation.Orientation;
+import org.firstinspires.ftc.robotcore.external.navigation.YawPitchRollAngles;
+
+// Generic robot class
+public class GearhoundsHardware extends Hardware {
+    public HardwareMap robotMap;
+
+// Examples Here
+/*  public DcMotorEx ExampleMotor;
+    public Servo ExampleServo;
+    public HuskyLens huskyLens;
+*/
+//Drivetrain Members
+    public DcMotorEx frontLeft;
+    public DcMotorEx backLeft;
+    public DcMotorEx frontRight;
+    public DcMotorEx backRight;
+
+    public static final int READ_PERIOD = 1;
+    public IMU imu;
+    public Orientation angles;
+    public Acceleration gravity;
+    private YawPitchRollAngles             lastAngles;
+    private static double                  globalAngle;
+
+    // 1000 ticks was roughly 18 in.
+    public static final double TICK_PER_INCH = 600.0/12.0;
+    public static final double MinPower = 0.3;
+
+    static final double FEET_PER_METER = 3.28084;
+
+    //Constructor
+    public GearhoundsHardware(){
+
+    }
+
+    // Override to set actual robot configuration
+    public void init(HardwareMap hwMap) {
+
+        // Save reference to Hardware map
+        robotMap = hwMap;
+    //Examples Here
+    /*
+        huskyLens = robotMap.get(HuskyLens.class, "huskyLens");
+        ExampleServo = robotMap.get(Servo.class, "ExampleServo");
+        ExampleMotor = robotMap.get(DcMotorEx.class, "ExampleMotor");
+        NameInCodeHere = robotMap.get(DeviceTypeHere.class, "DriverStationNameHere");
+    */
+    //Drivetrain Members
+
+        frontLeft = robotMap.get(DcMotorEx.class, "frontLeft");
+        frontLeft.setDirection(DcMotorSimple.Direction.FORWARD);
+        backLeft = robotMap.get(DcMotorImplEx.class, "backLeft");
+        backLeft.setDirection(DcMotorSimple.Direction.FORWARD);
+        frontRight = robotMap.get(DcMotorEx.class, "frontRight");
+        frontRight.setDirection(DcMotorSimple.Direction.FORWARD);
+        backRight = robotMap.get(DcMotorEx.class, "backRight");
+        backRight.setDirection(DcMotorSimple.Direction.FORWARD);
+
+        // Defines the REV Hub's internal IMU (Gyro)
+        imu = robotMap.get(IMU.class, "imu");
+
+        // Defines the parameters for the gyro (units)
+        IMU.Parameters imuParameters = new IMU.Parameters(
+                new RevHubOrientationOnRobot(
+                        RevHubOrientationOnRobot.LogoFacingDirection.RIGHT,
+                        RevHubOrientationOnRobot.UsbFacingDirection.UP
+                )
+        );
+        imu.initialize(imuParameters);
+        resetAngle();
+    }
+
+
+
+    public void resetAngle() {
+        imu.resetYaw();
+        lastAngles = imu.getRobotYawPitchRollAngles();
+        globalAngle = 0;
+    }
+
+    public double getAngle() {
+        YawPitchRollAngles angles = imu.getRobotYawPitchRollAngles();
+        double deltaAngle = angles.getYaw(AngleUnit.DEGREES) - lastAngles.getYaw(AngleUnit.DEGREES);
+        if (deltaAngle < -180)
+            deltaAngle += 360;
+        else if (deltaAngle > 180)
+            deltaAngle -= 360;
+        globalAngle += deltaAngle;
+        lastAngles = angles;
+        return globalAngle;
+    }
+
+}
