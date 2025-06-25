c391b2bf6e5237bf5d6d86119fc0530c46c6fbc6
Gearhounds9242 <ycds9242@gmail.com>
2025-06-25 14:14:07 -0400

    Commit test

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Utilities/GearhoundsHardware.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Utilities/GearhoundsHardware.java
@@ -29,7 +29,6 @@ public class GearhoundsHardware extends Hardware {
     public DcMotorEx backLeft;
     public DcMotorEx frontRight;
     public DcMotorEx backRight;
-
     public static final int READ_PERIOD = 1;
     public IMU imu;
     public Orientation angles;
